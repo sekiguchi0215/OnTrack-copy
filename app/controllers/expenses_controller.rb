2766d6a2528cf1d40d9b845c05593ce2052af8f9
vagrant <dsphere5296@gmail.com>
2021-01-15 14:32:58 +0000

    controllers/expenses_controller.rbの記述

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,2 +1,8 @@
 class ExpensesController < ApplicationController
+	def index
+		@category_id = params[:categroy_id].to_json
+		@paid_after = params[:paid_after].to_json
+		@has_data - Expense.count > 0
+		@categories = Category.all.order(:name).to_json
+	end
 end
