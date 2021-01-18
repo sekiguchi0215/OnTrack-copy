063558b874e654a2ff1711167bdaf1717d4806b4
vagrant <dsphere5296@gmail.com>
2021-01-18 15:24:04 +0000

    errors_controller.rbのコメント追加

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,14 +2,18 @@ class ErrorsController < ApplicationController
 	skip_before_action :require_login
 
 	def not_found
-		render status: 400
+		render status: 400 # ":bad_request"
+		# :status オプション
+		# Railsが返すレスポンスのHTTPステータスコードは自動的に生成される。（ほとんどの場合、 200 :ok となる。）
+		# :status オプションを使用することで、レスポンスのステータスコードを変更できる。
+		# 参考記事（https://railsguides.jp/layouts_and_rendering.html）
 	end
 
 	def unacceptable
-		render status: 422
+		render status: 422 # ":unprocessable_entity"
 	end
 
 	def internal_error
-		render status: 500
+		render status: 500 # ":internal_server_error"
 	end
 end
