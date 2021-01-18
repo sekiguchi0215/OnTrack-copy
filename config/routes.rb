d3b5b22229fa8c326cc8bb52284896c4e75f85dc
vagrant <dsphere5296@gmail.com>
2021-01-18 14:44:02 +0000

    config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,31 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "sessions#new"
+
+  resources :sessions, only: [:new, :create] do
+  	delete :destroy, on: :collection
+  end
+
+  resources :dashboard, only: [:index]
+  resources :insights, only: [:index]
+  resources :expenses, only: [:index]
+  resources :expense_uploads, only: [:new, :create]
+
+  namespace :api do
+  	namespace :vi do
+  		resources :expenses, only: [:index, :create, :destroy, :update]
+  		resources :categories, only: [:index, :create, :update, :destroy]
+  		resources :reports do
+  			get :year, on: :collection
+  			get :month, on: :collection
+  		end
+
+  		resources :goals, only: [:index] do
+  			put :update, on: :collection
+  		end
+  	end
+  end
+
+  get "/404", to: "errors#not_found"
+  get "/422", to: "errors#not_found"
+  get "/500", to: "errors#not_found"
 end
