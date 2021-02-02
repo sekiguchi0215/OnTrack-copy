cd142b56d1c14fc418de37b841c0a966ff54b0d9
vagrant <dsphere5296@gmail.com>
2021-02-02 16:26:57 +0000

    javascript/api/modules/expenses.jsの作成・記述

---
+++ app/javascript/api/modules/expenses.js
@@ -0,0 +1,18 @@
+import Base from "./base";
+
+const Expenses = {
+	list(params, opts = {}) {
+		return Base.get(`/expenses`, params, opts || {});
+	},
+	create(data, opts = {}) {
+		return Base.post(`/expenses`, data, opts || {});
+	},
+	delete(id, params, opts = {}) {
+		return Base.delete(`/expenses/${id}`, params, opts || {});
+	},
+	update(id, data, opts = {}) {
+		return Base.put(`/expenses/${id}`, data, opts || {});
+	},
+}
+
+export default Expenses;
